d8f01d1182bba9bd78a88d8167857d2db8bb07fd
Romulo-Sergio-RE <89258510+Romulo-Sergio-RE@users.noreply.github.com>
2022-10-28 16:12:30 -0300

    mudando na pasta config do banco

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://romulo:12345@alura.3u7w7jg.mongodb.net/alura-node")
+mongoose.connect("mongodb+srv://{name:senha}@alura.3u7w7jg.mongodb.net/alura-node")
 
 let db = mongoose.connection
 
